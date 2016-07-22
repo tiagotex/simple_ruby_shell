fca14d62009c54856fb464b582af695cd6679c9a
Tiago Teixeira <tiagocmtex@gmail.com>
2016-07-22 17:58:42 +0200

    Add auto complete
    Press tab to receive completion for directories and commands in history

--- shell.rb
+++ shell.rb
@@ -1,5 +1,18 @@
 require 'readline'
 
+complete = proc do |s|
+  directory_list = Dir.glob("#{s}*")
+
+  if !directory_list.empty?
+    directory_list
+  else
+    Readline::HISTORY.grep(/^#{Regexp.escape(s)}/)
+  end
+end
+
+Readline.completion_append_character = ' '
+Readline.completion_proc = complete
+
 loop do
   input = Readline.readline('> ', true)
 
